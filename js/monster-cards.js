79f6a96d527d367b10246bd55edcf3aef7fc8f06
marycas <mary.castellanes@vpl.ca>
2022-06-23 15:00:24 -0700

    Applied flexbox to resources used blocks

--- js/monster-cards.js
+++
@@ -1,9 +0,0 @@
-// Monster Cards Flip
-var card = document.querySelectorAll(".monster-flip-card-test main ul li");
-var i;
-
-for (i = 0; i < card.length; i++) {
-  card[i].addEventListener("click", function() {
-    this.classList.toggle("flipped");
-  });
-}
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Converts callout blocks into a clickable area
+var resource = document.querySelectorAll(".service-node__resources-used .resource-type-node__image");
+var resourceLink = resource.innerHTML;
+var i;
+
+for (i = 0; i < resource.length; i++) {
+  resource[i].addEventListener("click", function() {
+    console.log(resourceLink);
+  });
+}
