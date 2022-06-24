afeb234712c8ae964f77764e128bae1b661c8d71
marycas <mary.castellanes@vpl.ca>
2022-06-24 11:25:51 -0700

    Made resources smaller

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Converts callout blocks into a clickable area
 var resource = document.querySelectorAll(".service-node__resources-used .resource-type-node__image");
-var resourceLink = resource.innerHTML;
+var resourceLink =  document.querySelectorAll(".service-node__resources-used a").innerHTML;
 var i;
 
 for (i = 0; i < resource.length; i++) {
