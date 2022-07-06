b0358419fb052a0ed48942f4cf81c2dd66ca0bef
marycas <mary.castellanes@vpl.ca>
2022-07-06 09:55:18 -0700

    Scripts for making services cards clickable

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // Converts callout blocks into a clickable area
-var resource = document.querySelectorAll(".service-node__resources-used .resource-type-node__image");
-var resourceLink =  document.querySelectorAll(".service-node__resources-used a").innerHTML;
+var service = document.querySelectorAll(".card .face__front");
 var i;
 
-for (i = 0; i < resource.length; i++) {
-  resource[i].addEventListener("click", function() {
-    console.log(resourceLink);
+for (i = 0; i < service.length; i++) {
+  var serviceLink = service[0].children[0].querySelector('a').href;
+  service[i].addEventListener("click", function() {
+    location.href = serviceLink;
   });
 }
