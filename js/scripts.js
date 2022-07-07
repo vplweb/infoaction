b05e2531113d414d0ea392ef353e86cc2a855a29
marycas <mary.castellanes@vpl.ca>
2022-07-07 11:59:14 -0700

    Commented out services scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-// Converts callout blocks into a clickable area
-var service = document.querySelectorAll(".card .face__front");
+// // Converts callout blocks into a clickable area
+// var service = document.querySelectorAll(".card .face__front");
 
-service.forEach((item, index) => {
-  item.onclick = function() {
-    var serviceLink = item.querySelector('a').href;
-    location.href = serviceLink;
-  }   
-});
+// service.forEach((item, index) => {
+//   item.onclick = function() {
+//     var serviceLink = item.querySelector('a').href;
+//     location.href = serviceLink;
+//   }   
+// });
